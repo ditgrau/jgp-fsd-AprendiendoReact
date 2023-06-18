575a57e38ca2e20fc994f4cf945c31f1e62ac2dd
DitGrau <juditgraup@gmail.com>
2023-06-18 20:42:19 +0200

    renderizado condicional de las peliculas

--- 2023/project/04-buscaPelis/04-buscaPelis/src/App.jsx
+++ 2023/project/04-buscaPelis/04-buscaPelis/src/App.jsx
@@ -4,20 +4,41 @@ import withoutResults from './mocks/no-results.json'
 import './App.css'
 
 function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   const [count, setCount] = useState(0)
 
   return (
     <div className='page'>
-    <header>
-      <h1>BUSCADOR DE PELICULAS</h1>
-      <form className='form'>
-        <input placeholder='Lilo & Stitch, Buscando a Nemo...'></input>
-        <button type='submit'>Buscar</button>
-      </form>
-    </header>
-    <main>
-  AQUI VAN LAS PELIS
-    </main>
+      <header>
+        <h1>BUSCADOR DE PELICULAS</h1>
+        <form className='form'>
+          <input placeholder='Lilo & Stitch, Buscando a Nemo...'></input>
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        AQUI VAN LAS PELIS
+        {
+          hasMovies
+            ? (
+              <ul>
+                {
+                  movies.map(movie => {
+                    <li key={movie.imdbID}>
+                      <h2>{movie.Title}</h2>
+                      <p>{movie.Type}</p>
+                      <img src={movie.Poster} alt={movie.Title}/>
+                    </li>
+                  })
+                }
+              </ul>
+            )
+            : (
+             <p>No se han encontrado resultados</p> 
+            )
+  }
+      </main>
     </div>
   )
 }
