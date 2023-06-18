3585cc9862cf5c0d43c1a845c860cddffae339b1
DitGrau <juditgraup@gmail.com>
2023-06-18 15:47:43 +0200

    añadido el stado del turno

--- 2023/project/01-tikTakToe/01-tikTakToe/src/App.jsx
+++ 2023/project/01-tikTakToe/01-tikTakToe/src/App.jsx
@@ -6,9 +6,11 @@ const TURNS = {
 
 
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({children, isSelected, updateBoard, index}) => {
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`
   return(
-    <div className='square'>
+    <div className={className}>
       {children}
     </div>
   )
@@ -16,7 +18,10 @@ const Square = ({children, updateBoard, index}) => {
 
 function App() {
 
-  const [board, setBoard] = useState(['o', 'x', 'x', 'x', 'o', 'x', 'o', 'x', 'o'])
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
 
   return (
     <main className='board'>
@@ -32,6 +37,11 @@ function App() {
           )
         })
       }</section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+
+      </section>
     </main>
   )
 }
