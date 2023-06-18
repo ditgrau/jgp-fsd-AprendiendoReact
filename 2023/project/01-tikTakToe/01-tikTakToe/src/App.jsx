69b73f2a6c60d2752a148dbd9c417cc5de199c93
DitGrau <juditgraup@gmail.com>
2023-06-18 15:06:15 +0200

    Tablero ya formado

--- 2023/project/01-tikTakToe/01-tikTakToe/src/App.jsx
+++ 2023/project/01-tikTakToe/01-tikTakToe/src/App.jsx
@@ -1,12 +1,34 @@
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
 
+const board = Array(9).fill(null)
+
+const Square = ({children, updateBoard, index}) => {
+  return(
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
 
 function App() {
-  
-
   return (
-    <>
-      TIC TAC TOE
-    </>
+    <main className='board'>
+      <h1>TIC TAC TOE</h1>
+      <section className='game'>{
+        board.map((_, index) => {
+          return (
+            <Square
+            key={index}
+            index={index}>
+                {index}
+            </Square>
+          )
+        })
+      }</section>
+    </main>
   )
 }
 
