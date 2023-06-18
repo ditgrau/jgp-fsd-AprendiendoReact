fe8745b6418e7d2565539f5457d0c7d76fe0b50a
DitGrau <juditgraup@gmail.com>
2023-06-18 14:04:49 +0200

    FIX: hover con texto para el boton 'dejar de seguir'

--- 2023/project/00-hola-mundo/src/FollowCard.jsx
+++ 2023/project/00-hola-mundo/src/FollowCard.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import './FollowCard.css'
 
-export function FollowCard({ children , alias = 'unknown', initialIsFollowing }) {
+export function FollowCard({ children, alias = 'unknown', initialIsFollowing }) {
 
-const [isFollowing, setIsFollowing] = useState(initialIsFollowing); //useState devuelve un array de dos posiciones con valor y funcion que actualiza el valor
-// lo de dentro del parentesis de useSatet es el valor INICIAL
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing); //useState devuelve un array de dos posiciones con valor y funcion que actualiza el valor
+    // lo de dentro del parentesis de useSatet es el valor INICIAL
 
-const text = isFollowing ? 'Siguiendo' : 'Seguir' //ternaria, condicional
-//cuando en el renderizado, en la App, isFollowing es true: 'siguiendo' 
-const buttonClassName = isFollowing //el boton es = al estado 'isFollowing'???
-? 'tw-followCard-button is-following' //SÍ, entonces css
-: 'tw-followCard-button' //NO, entonces este css
+    const text = isFollowing ? 'Siguiendo' : 'Seguir' //ternaria, condicional
+    //cuando en el renderizado, en la App, isFollowing es true: 'siguiendo' 
+    const buttonClassName = isFollowing //el boton es = al estado 'isFollowing'???
+        ? 'tw-followCard-button is-following' //SÍ, entonces css
+        : 'tw-followCard-button' //NO, entonces este css
 
-const handleClick = () => {
-    setIsFollowing(!isFollowing) //la funcion del click va cambiando de true a false y viceversa
-}
+    const handleClick = () => {
+        setIsFollowing(!isFollowing) //la funcion del click va cambiando de true a false y viceversa
+    }
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
@@ -26,7 +26,8 @@ const handleClick = () => {
             </header>
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
