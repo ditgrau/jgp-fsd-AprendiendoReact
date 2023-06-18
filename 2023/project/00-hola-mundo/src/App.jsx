3df2fa060c75046bc989d91a408bee4d4178a797
DitGrau <juditgraup@gmail.com>
2023-06-18 12:13:00 +0200

    REFACTOR: prop boleano, isFollowing

--- 2023/project/00-hola-mundo/src/App.jsx
+++ 2023/project/00-hola-mundo/src/App.jsx
@@ -3,10 +3,11 @@ import { FollowCard } from './FollowCard'
 
 export function App() {
     return (
-        <div className='containerCards'> 
-            <FollowCard userName='Judit Grau' alias='ditGrau' />
-            <FollowCard userName='Miguel Angel Duran' alias='midudev' />
-            <FollowCard userName='Daniel Profe' alias='datata' />
+        <div className='containerCards'>
+            <FollowCard userName='Judit Grau' alias='ditGrau' isFollowing/>
+            <FollowCard userName='Miguel Angel Duran' alias='midudev' isFollowing={false}/>
+            <FollowCard userName='Daniel Profe' alias='datata' isFollowing/>
+            {/* isFollowing es un boleano, al ponerlo sin value por default es true, para darle valor: expresion, entre LLAVES. */}
         </div>
     )
 }
