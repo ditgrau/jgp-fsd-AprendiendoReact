2068bcf41b82f3d6806990d2158695132e06dd22
DitGrau <juditgraup@gmail.com>
2023-06-18 09:58:52 +0200

    "STYLE: estilada la card"

--- 2023/project/00-hola-mundo/src/App.jsx
+++ 2023/project/00-hola-mundo/src/App.jsx
@@ -1,14 +1,20 @@
+import './App.css'
 
 export function App() {
     return (
-        <article>
-            <header>
-                <img alt='avatar' src='https://unavatar.io/ditgrau'></img>
-                <div>
-                    <strong>Judit Grau Puigdoller</strong>
-                    <span>@ditGrau</span>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-img'alt='avatar' src='https://unavatar.io/ditgrau'></img>
+                <div className='tw-followCard-info'>
+                    <strong className='tw-followCard-infoUserName'>Judit Grau Puigdollers</strong>
+                    <span className='tw-followCard-infoAliasName'>@ditGrau</span>
                 </div>
             </header>
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
         </article>
 
     )
