f7be6dc80817a116a3f28197772ab388e74af1fb
DitGrau <juditgraup@gmail.com>
2023-06-18 12:39:03 +0200

    REFACTOR: uso del children como prop

--- 2023/project/00-hola-mundo/src/App.jsx
+++ 2023/project/00-hola-mundo/src/App.jsx
@@ -4,9 +4,15 @@ import { FollowCard } from './FollowCard'
 export function App() {
     return (
         <div className='containerCards'>
-            <FollowCard userName='Judit Grau' alias='ditGrau' isFollowing/>
-            <FollowCard userName='Miguel Angel Duran' alias='midudev' isFollowing={false}/>
-            <FollowCard userName='Daniel Profe' alias='datata' isFollowing/>
+            <FollowCard alias='ditGrau' isFollowing>
+                Judit Grau
+            </FollowCard>
+            <FollowCard isFollowing={false}>
+                Miguel Angel Duran
+            </FollowCard>
+            <FollowCard alias='datata' isFollowing>
+                Daniel Profe
+            </FollowCard>
             {/* isFollowing es un boleano, al ponerlo sin value por default es true, para darle valor: expresion, entre LLAVES. */}
         </div>
     )
--- 2023/project/00-hola-mundo/src/FollowCard.jsx
+++ 2023/project/00-hola-mundo/src/FollowCard.jsx
@@ -1,14 +1,14 @@
 import './FollowCard.css'
 
-export function FollowCard({ userName , alias , isFollowing}) {
+export function FollowCard({ children , alias = 'unknown', isFollowing}) {
 
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-img' alt='avatar' src={`https://unavatar.io/${alias}`}></img>
                 <div className='tw-followCard-info'>
-                    <strong className='tw-followCard-infoUserName'>{userName}</strong>
-                    <span className='tw-followCard-infoAliasName'>{alias}</span>
+                    <strong className='tw-followCard-infoUserName'>{children}</strong>
+                    <span className='tw-followCard-infoAliasName'>@{alias}</span>
                 </div>
             </header>
             <aside>
