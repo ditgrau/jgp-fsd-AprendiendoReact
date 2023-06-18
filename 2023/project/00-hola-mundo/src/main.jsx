3a444ad827a48aabc4e3ccd1ae87a7976fa12eb6
DitGrau <juditgraup@gmail.com>
2023-06-18 09:11:55 +0200

    CHORE: ara si, empezamos

--- 2023/project/00-hola-mundo/src/main.jsx
+++ 2023/project/00-hola-mundo/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render( 'Hola Mundo'
-  // const root = ReactDOM.createRoot(document.getElementById('root'))
-  // root.render ()
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render()  
