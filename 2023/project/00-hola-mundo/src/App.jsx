f90b95735073e69ff3d4cb822554cb45ae2b6884
DitGrau <juditgraup@gmail.com>
2023-06-18 13:37:58 +0200

    Otro useState a nivel de app para probar

--- 2023/project/00-hola-mundo/src/App.jsx
+++ 2023/project/00-hola-mundo/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
-import { FollowCard } from './FollowCard'
+import { useState } from 'react';
+import './App.css';
+import { FollowCard } from './FollowCard';
 
 
 export function App() {
 
-    const ditgrau = { alias: 'ditGrau'};
+    const [name, setName] = useState('datata')
+    const ditgrau = { alias: 'ditGrau' };
 
     return (
         <div className='containerCards'>
@@ -14,10 +16,14 @@ export function App() {
             <FollowCard >
                 Miguel Angel Duran
             </FollowCard>
-            <FollowCard alias='datata'>
+            <FollowCard alias={name}>
                 Daniel Profe
             </FollowCard>
             {/* isFollowing es un boleano, al ponerlo sin value por default es true, para darle valor: expresion, entre LLAVES. */}
+            <button onClick={() => setName('rakata')}>
+                {/* onclick, callback */}
+                Cambio nombre
+            </button>
         </div>
     )
 }
