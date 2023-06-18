2f75f44e49d0864d89477c8928efe4a8e8b9f4fd
DitGrau <juditgraup@gmail.com>
2023-06-18 09:24:06 +0200

    REFACTOR: parametrizado el texto del boton

--- 2023/project/00-hola-mundo/src/main.jsx
+++ 2023/project/00-hola-mundo/src/main.jsx
@@ -5,19 +5,19 @@ import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // creacion de un "componente"
-const createButton = () => {
+const createButton = ({text}) => {
 return <button>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
-      Hola boton</button>
+      {text}</button>
 }
 
 root.render( //el metodo render, espera solo un argumento
   <React.Fragment>
-    {createButton()} 
-    {createButton()} 
-    {createButton()} 
+    {createButton({text: 'button 1'})} 
+    {createButton({text: 'button 2'})} 
+    {createButton({text: 'button 3'})} 
     
   </React.Fragment>
 )  
