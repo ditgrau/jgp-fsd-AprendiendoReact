9bcb662234dbcc99636f33154378c6a17fcae58e
DitGrau <juditgraup@gmail.com>
2023-06-18 18:58:36 +0200

    mouse funcionando

--- 2023/project/02-useEffect/src/App.jsx
+++ 2023/project/02-useEffect/src/App.jsx
@@ -3,19 +3,41 @@ import './App.css'
 
 function App() {
 
-const [enabled , setEnable] = useState(false)
-
+  const [enabled, setEnable] = useState(false)
+const [position, setPosition] = useState({x: 0, y: 0})
   useEffect(() => {
-    console.log ('effect ', {enabled})
+    console.log('effect ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x:clientX , y:clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
   }, [enabled]) //dependencia del efecto. Este solo se ejecutara cuando el valor de la dependencia cambie
 
   return (
-    <>
-      <h2>PROYECTO POINTER</h2>
-      <button onClick={()=> setEnable(!enabled)
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#ff8000',
+        borderRadius: '50%',
+        opacity: 0.6,
+        pointerEvent: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+      </div>
+      <button onClick={() => setEnable(!enabled)
       }>
-        {enabled ? 'Desactivar' :'Activar'}</button>
-    </>
+        {enabled ? 'Desactivar' : 'Activar'}</button>
+    </main>
   )
 }
 
